85924863b7d8f390400fe16489c4ea895c71ad81
dirui <dirui@bujiaban.com>
2022-10-21 05:30:38 +0800

    no message

---
+++ advanced/api/modules/v1/models/MetaKnightSearch.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace api\modules\v1\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use api\modules\v1\models\MetaKnight;
+
+/**
+ * MetaKnightSearch represents the model behind the search form of `api\modules\v1\models\MetaKnight`.
+ */
+class MetaKnightSearch extends MetaKnight
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'verse_id', 'knight_id', 'user_id'], 'integer'],
+            [['info', 'create_at'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = MetaKnight::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'verse_id' => $this->verse_id,
+            'knight_id' => $this->knight_id,
+            'user_id' => $this->user_id,
+            'create_at' => $this->create_at,
+        ]);
+
+        $query->andFilterWhere(['like', 'info', $this->info]);
+
+        return $dataProvider;
+    }
+}
