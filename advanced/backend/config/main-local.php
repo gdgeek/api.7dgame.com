4efbf830cde001055f310dda144c3f274df53f88
dirui <dirui@bujiaban.com>
2023-01-22 23:49:56 +0800

    换成debug

--- advanced/backend/config/main-local.php
+++ advanced/backend/config/main-local.php
@@ -1,9 +1,25 @@
 <?php
-return [
+
+$config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'HE6s7tJ2yDTNobDD4UhfEyPuxccGhuJX',
+            'cookieValidationKey' => 'SddXmkw307oOI7PcY5E0O6MCPW3mpORb',
         ],
     ],
 ];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
