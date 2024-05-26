932ddebf52193979f80cd8711c0234909e92f9d6
gdgeek <dirui1@yeah.net>
2024-05-26 14:44:40 +0800

    no message

--- advanced/api/modules/v1/models/Meta.php
+++ advanced/api/modules/v1/models/Meta.php
@@ -87,7 +87,6 @@ class Meta extends \yii\db\ActiveRecord
         unset($fields['updater_id']);
         unset($fields['updated_at']);
         unset($fields['created_at']);
-        //unset($fields['info']);
         $fields['image'] = function () {
             return $this->image;
         };
