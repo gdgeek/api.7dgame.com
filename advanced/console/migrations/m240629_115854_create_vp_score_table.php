d7dc83ec5dd5d504f40f3dce817f68d399b8b46b
gdgeek <dirui1@yeah.net>
2024-06-29 20:16:20 +0800

    no message

--- advanced/console/migrations/m240629_115854_create_vp_score_table.php
+++ advanced/console/migrations/m240629_115854_create_vp_score_table.php
@@ -3,43 +3,43 @@
 use yii\db\Migration;
 
 /**
- * Handles the creation of table `{{%score}}`.
+ * Handles the creation of table `{{%vp_score}}`.
  * Has foreign keys to the tables:
  *
  * - `{{%verse}}`
  */
-class m240629_102619_create_score_table extends Migration
+class m240629_115854_create_vp_score_table extends Migration
 {
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
-
         if ($this->db->driverName === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
-        $this->createTable('{{%score}}', [
+
+        $this->createTable('{{%vp_score}}', [
             'id' => $this->primaryKey(),
             'verse_id' => $this->integer()->notNull(),
-            'player_id' => $this->string()->notNull(),
-            'score' => $this->integer()->defaultValue(0),
-            'created_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP')->notNull(),
-            'updated_at' => $this->timestamp(),
+            'player_id' => $this->string(),
+            'score' => $this->integer(),
+            'created_at' => $this->dateTime(),
+            'updated_at' => $this->dateTime(),
         ], $tableOptions);
 
         // creates index for column `verse_id`
         $this->createIndex(
-            '{{%idx-score-verse_id}}',
-            '{{%score}}',
+            '{{%idx-vp_score-verse_id}}',
+            '{{%vp_score}}',
             'verse_id'
         );
 
         // add foreign key for table `{{%verse}}`
         $this->addForeignKey(
-            '{{%fk-score-verse_id}}',
-            '{{%score}}',
+            '{{%fk-vp_score-verse_id}}',
+            '{{%vp_score}}',
             'verse_id',
             '{{%verse}}',
             'id',
@@ -54,16 +54,16 @@ class m240629_102619_create_score_table extends Migration
     {
         // drops foreign key for table `{{%verse}}`
         $this->dropForeignKey(
-            '{{%fk-score-verse_id}}',
-            '{{%score}}'
+            '{{%fk-vp_score-verse_id}}',
+            '{{%vp_score}}'
         );
 
         // drops index for column `verse_id`
         $this->dropIndex(
-            '{{%idx-score-verse_id}}',
-            '{{%score}}'
+            '{{%idx-vp_score-verse_id}}',
+            '{{%vp_score}}'
         );
 
-        $this->dropTable('{{%score}}');
+        $this->dropTable('{{%vp_score}}');
     }
 }
