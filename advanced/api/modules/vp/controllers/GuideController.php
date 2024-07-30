2fa325644765df6578ef3d82563d368f8240451f
gdgeek <dirui1@yeah.net>
2024-07-30 14:54:57 +0800

    no message

--- advanced/api/modules/vp/controllers/GuideController.php
+++ advanced/api/modules/vp/controllers/GuideController.php
@@ -37,7 +37,7 @@ class GuideController extends ActiveController
     public function actions()
     {
         $actions = parent::actions();
-       unset($actions['index']);
+        unset($actions['index']);
         unset($actions['create']);
         unset($actions['update']);
         unset($actions['delete']);
@@ -45,57 +45,6 @@ class GuideController extends ActiveController
         unset($actions['view']);
         return $actions;
     }
-    /*
-    public function actionUpgrade(){
-        $post = \Yii::$app->request->post();
-
-        $cache = \Yii::$app->cache;
-        $cache->set('log', $post);
-        if(isset($post["data"])){
-            $data = json_decode($post["data"], true);
-        }else{
-            throw new \Exception("no data");
-        }
-        if(!isset($data['version'])){
-            throw new \Exception("no version");
-
-        }
-       
-        $result = version_compare($data['version'], $version2 = '1.1.9');
-        if($result > 0){
-            throw new \Exception("version too low" .$result);
-        }
-        if(!isset($data["scenes"])){
-            throw new \Exception("no scenes");
-        }
-        $scenes = $data["scenes"];
-        if(!is_array($scenes)){
-            throw new \Exception("scenes not array");
-        }
-        for($n = 0; $n < count($scenes); $n++){
-            $scene = $scenes[$n];
-            if(!isset($scene["levels"])){
-                throw new \Exception("no scene id");
-            }
-            $levels = $scene["levels"];
-            if(!is_array($levels)){
-                throw new \Exception("levels not array");
-            }
-            for($i = 0; $i < count($levels); $i++){
-               $level = $levels[$i];
-               $datas = new ActiveDataProvider([
-                    'query' => \api\modules\vp\models\Guide::find(),
-                    'pagination' => [
-                        'pageSize' => false,
-                    ]
-               ]);
-               return $datas;
-               
-
-            }
-        }
-        return $data;
-    }*/
 
     public function actionIndex()
     {
