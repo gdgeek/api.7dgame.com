5af96f3637a64cc1f944e572887a579c89aea86d
gdgeek <dirui1@yeah.net>
2024-08-05 22:42:35 +0800

    no message

--- advanced/api/modules/vp/controllers/Oauth2Controller.php
+++ advanced/api/modules/vp/controllers/Oauth2Controller.php
@@ -37,13 +37,12 @@ class Oauth2Controller extends \yii\rest\Controller{
         $idToken = Yii::$app->request->post('id_token');
         $user = Yii::$app->request->post('user');
 
-
         // 确保返回 HTTP 200 状态码
         Yii::$app->response->statusCode = 200;
         // 记录收到的数据用于调试
         //file_put_contents('apple_login.log', print_r(Yii::$app->request->post(), true), FILE_APPEND);
         $cache = \Yii::$app->cache;
-        $cache->set('apple', Yii::$app->request->post());
+        $cache->set('apple', ['ip'=>$this->getRealIpAddr(),'get'=>Yii::$app->request->get(),'post'=>Yii::$app->request->post()]);
         // 返回 HTTP 200 状态码和 JSON 响应
         return ['status' => 'success', 'message' => 'Data received successfully'];
     }
