84a96f712308db78c61e3d7c0a8e9ec81a155640
gdgeek <dirui1@yeah.net>
2024-08-05 23:42:37 +0800

    no message

--- advanced/api/modules/vp/controllers/Oauth2Controller.php
+++ advanced/api/modules/vp/controllers/Oauth2Controller.php
@@ -9,7 +9,22 @@ class Oauth2Controller extends \yii\rest\Controller{
     public function behaviors()
     {
         $behaviors = parent::behaviors();
-       
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::class,
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Credentials' => null,
+                'Access-Control-Max-Age' => 86400,
+                'Access-Control-Expose-Headers' => [
+                    'X-Pagination-Total-Count',
+                    'X-Pagination-Page-Count',
+                    'X-Pagination-Current-Page',
+                    'X-Pagination-Per-Page',
+                ],
+            ],
+        ];
         return $behaviors;
     }
     public function actionClear(){
@@ -29,6 +44,8 @@ class Oauth2Controller extends \yii\rest\Controller{
         }
         return $ip;
     }
+    //https://appleid.apple.com/auth/authorize?client_id=com.voxelparty.www&redirect_uri=https%3A%2F%2Fapi.voxelparty.com%2Fvp%2Foauth2%2Fapple-id-login&response_type=code%20id_token&state=your-state&scope=name%20email&response_mode=web_message&frame_id=4de2e626-da69-4b09-a8fd-54a9a710def1&m=12&v=1.5.5
+    //https://appleid.apple.com/auth/authorize?scope=email&state=a9583c14408af68ac05cbfed3a8274ef&response_type=code&approval_prompt=auto&redirect_urihttps%3A%2F%2Fapi.voxelparty.com%2Fvp%2Foauth2%2Fapple-id-login&client_id=com.voxelparty.www&response_mode=form_post
     public function actionAppleIdLogin(){
         Yii::$app->response->format = Response::FORMAT_JSON;
 
