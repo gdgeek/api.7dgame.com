01b8b53533339c35ce82f0244a59e978755cddc7
gdgeek <dirui1@yeah.net>
2024-08-05 16:50:38 +0800

    更新下内容

--- advanced/api/modules/vp/controllers/Oauth2Controller.php
+++ advanced/api/modules/vp/controllers/Oauth2Controller.php
@@ -2,7 +2,9 @@
 namespace api\modules\vp\controllers;
 use api\modules\vp\models\Token;
 use yii\rest\ActiveController;
+use Yii;
 
+use yii\web\Response;
 class Oauth2Controller extends \yii\rest\Controller{
     public function behaviors()
     {
@@ -12,10 +14,27 @@ class Oauth2Controller extends \yii\rest\Controller{
     }
     
     public function actionAppleIdLogin(){
-        $cache = \Yii::$app->cache;
-        $cache->set('apple', ["get"=>\Yii::$app->request->get(),"post"=> \Yii::$app->request->post()]);
+        Yii::$app->response->format = Response::FORMAT_JSON;
 
-        return "apple";
+        if (Yii::$app->request->isPost) {
+            $code = Yii::$app->request->post('code');
+            $idToken = Yii::$app->request->post('id_token');
+            $user = Yii::$app->request->post('user');
+
+            // 记录收到的数据，便于调试
+          //  Yii::info(Yii::$app->request->post(), 'apple_login');
+
+            $cache = \Yii::$app->cache;
+            $cache->set('apple', Yii::$app->request->post());
+            $cache->set('apple_code', "$code");
+            // 返回 HTTP 200 状态码
+            Yii::$app->response->statusCode = 200;
+            return ['status' => 'success', 'message' => 'Data received successfully'];
+        }
+
+        // 处理非 POST 请求
+        Yii::$app->response->statusCode = 405;
+        return ['status' => 'error', 'message' => 'Method Not Allowed'];
     }
     public function actionIndex(){
         $cache = \Yii::$app->cache;
