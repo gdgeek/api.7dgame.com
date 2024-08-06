91db9ede033e9b85098a7855f9b485ce9b3602aa
gdgeek <dirui1@yeah.net>
2024-08-06 15:54:07 +0800

    no message

--- advanced/api/modules/vp/controllers/Oauth2Controller.php
+++ advanced/api/modules/vp/controllers/Oauth2Controller.php
@@ -4,6 +4,7 @@ use api\modules\vp\models\Token;
 use yii\rest\ActiveController;
 use Yii;
 
+use yii\helpers\Url;
 use yii\web\Response;
 class Oauth2Controller extends \yii\rest\Controller{
     public function behaviors()
@@ -28,17 +29,60 @@ class Oauth2Controller extends \yii\rest\Controller{
         return $behaviors;
     }
     public function actionTest(){
-
+        $currentUrlWithoutParams2 = Yii::$app->request->getHostInfo() . '/' . Yii::$app->request->getPathInfo();
+        return  $currentUrlWithoutParams2;
+        return Url::current([], true);
         \Firebase\JWT\JWT::$leeway = 60;
+        
         $provider = new \League\OAuth2\Client\Provider\Apple([
             'clientId'          => 'com.voxelparty.www',
             'teamId'            => 'PK435YWZ25', // 1A234BFK46 https://developer.apple.com/account/#/membership/ (Team ID)
             'keyFileId'         => 'Q3R52SBK3X', // 1ABC6523AA https://developer.apple.com/account/resources/authkeys/list (Key ID)
-            'keyFilePath'       => '{apple-key-file-path}', // __DIR__ . '/AuthKey_1ABC6523AA.p8' -> Download key above
-            'redirectUri'       => 'https://example.com/callback-url',
+            'keyFilePath'       => getenv('APPLE_AUTH_KEY'), // __DIR__ . '/AuthKey_1ABC6523AA.p8' -> Download key above
+            'redirectUri'       => 'https://api.4mr.cn/vp/oauth2/apple-id-login',
         ]);
-        
-        return Yii::$app->basePath;
+        if (!isset($_POST['code'])) {
+
+            // If we don't have an authorization code then get one
+            $authUrl = $provider->getAuthorizationUrl();
+            $_SESSION['oauth2state'] = $provider->getState();
+            header('Location: '.$authUrl);
+            exit;
+
+        // Check given state against previously stored one to mitigate CSRF attack
+        } elseif (empty($_POST['state']) || ($_POST['state'] !== $_SESSION['oauth2state'])) {
+
+            unset($_SESSION['oauth2state']);
+            exit('Invalid state');
+
+        } else {
+
+            // Try to get an access token (using the authorization code grant)
+            /** @var AppleAccessToken $token */
+            $token = $provider->getAccessToken('authorization_code', [
+                'code' => $_POST['code']
+            ]);
+
+            // Optional: Now you have a token you can look up a users profile data
+            // Important: The most details are only visible in the very first login!
+            // In the second and third and ... ones you'll only get the identifier of the user!
+            try {
+
+                // We got an access token, let's now get the user's details
+                $user = $provider->getResourceOwner($token);
+
+                // Use these details to create a new profile
+                printf('Hello %s!', $user->getFirstName());
+
+            } catch (Exception $e) {
+
+                // Failed to get user details
+                exit(':-(');
+            }
+
+            // Use this to interact with an API on the users behalf
+            echo $token->getToken();
+        }
     }
     public function actionClear(){
         $cache = \Yii::$app->cache;
@@ -63,6 +107,60 @@ class Oauth2Controller extends \yii\rest\Controller{
     //https://appleid.apple.com/auth/authorize?client_id=com.voxelparty.www&redirect_uri=https%3A%2F%2Fapi.voxelparty.com%2Fvp%2Foauth2%2Fapple-id-login&response_type=code%20id_token&state=your-state&scope=name%20email&response_mode=web_message&frame_id=4de2e626-da69-4b09-a8fd-54a9a710def1&m=12&v=1.5.5
     //https://appleid.apple.com/auth/authorize?scope=email&state=a9583c14408af68ac05cbfed3a8274ef&response_type=code&approval_prompt=auto&redirect_urihttps%3A%2F%2Fapi.voxelparty.com%2Fvp%2Foauth2%2Fapple-id-login&client_id=com.voxelparty.www&response_mode=form_post
     public function actionAppleIdLogin(){
+
+        $redirectUri = Yii::$app->request->getHostInfo() . '/' . Yii::$app->request->getPathInfo();
+        \Firebase\JWT\JWT::$leeway = 60;
+        
+        $provider = new \League\OAuth2\Client\Provider\Apple([
+            'clientId'          => getenv('APPLE_CLIENT_ID'), // com.voxelparty.www
+            'teamId'            => getenv('APPLE_AUTH_TEAM_ID') , // 1A234BFK46 https://developer.apple.com/account/#/membership/ (Team ID)
+            'keyFileId'         => getenv('APPLE_AUTH_KEY_IDs') , // 1ABC6523AA https://developer.apple.com/account/resources/authkeys/list (Key ID)
+            'keyFilePath'       => getenv('APPLE_AUTH_KEY'), // __DIR__ . '/AuthKey_1ABC6523AA.p8' -> Download key above
+            'redirectUri'       => $redirectUri,
+        ]);
+        if (!isset($_POST['code'])) {
+
+            // If we don't have an authorization code then get one
+            $authUrl = $provider->getAuthorizationUrl();
+            $_SESSION['oauth2state'] = $provider->getState();
+            header('Location: '.$authUrl);
+            exit;
+
+        // Check given state against previously stored one to mitigate CSRF attack
+        } elseif (empty($_POST['state']) || ($_POST['state'] !== $_SESSION['oauth2state'])) {
+
+            unset($_SESSION['oauth2state']);
+            exit('Invalid state');
+
+        } else {
+
+            // Try to get an access token (using the authorization code grant)
+            /** @var AppleAccessToken $token */
+            $token = $provider->getAccessToken('authorization_code', [
+                'code' => $_POST['code']
+            ]);
+
+            // Optional: Now you have a token you can look up a users profile data
+            // Important: The most details are only visible in the very first login!
+            // In the second and third and ... ones you'll only get the identifier of the user!
+            try {
+
+                // We got an access token, let's now get the user's details
+                $user = $provider->getResourceOwner($token);
+
+                // Use these details to create a new profile
+                printf('Hello %s!', $user->getFirstName());
+
+            } catch (Exception $e) {
+
+                // Failed to get user details
+                exit(':-(');
+            }
+
+            // Use this to interact with an API on the users behalf
+            echo $token->getToken();
+        }
+        /*
         Yii::$app->response->format = Response::FORMAT_JSON;
 
         // 获取 POST 数据
@@ -77,7 +175,7 @@ class Oauth2Controller extends \yii\rest\Controller{
         $cache = \Yii::$app->cache;
         $cache->set('apple', ['ip'=>$this->getRealIpAddr(),'get'=>Yii::$app->request->get(),'post'=>Yii::$app->request->post()]);
         // 返回 HTTP 200 状态码和 JSON 响应
-        return Yii::$app->response->redirect($state, 301);
+        return Yii::$app->response->redirect($state, 301);*/
        // return ['status' => 'success', 'message' => 'Data received successfully'];
     }
     public function actionIndex(){
