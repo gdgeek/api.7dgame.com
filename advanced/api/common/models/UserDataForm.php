398d0eca965ae70bac51007f955a0f6748bcc2d6
gdgeek <dirui1@yeah.net>
2024-08-28 11:52:11 +0800

    no message

--- advanced/api/common/models/UserDataForm.php
+++ advanced/api/common/models/UserDataForm.php
@@ -2,37 +2,38 @@
 
 namespace api\common\models;
 
+use api\modules\v1\components\Validator\JsonValidator;
 use api\modules\v1\models\File;
 use yii\base\Model;
 
 /**
- * This is the model class for table "verse".
- *
- */
+* This is the model class for table "verse".
+*
+*/
 class UserDataForm extends Model
 {
     public $nickname;
     public $avatar_id;
     public $info;
-
+    
     private $_user;
-
+    
     /**
-     * Creates a form model with given token.
-     *
-     * @param string $token
-     * @param array $config name-value pairs that will be used to initialize the object properties
-     * @throws InvalidArgumentException if token is empty or not valid
-     */
+    * Creates a form model with given token.
+    *
+    * @param string $token
+    * @param array $config name-value pairs that will be used to initialize the object properties
+    * @throws InvalidArgumentException if token is empty or not valid
+    */
     public function __construct($user, array $config = [])
     {
         $this->_user = $user;
         $info = $this->_user->userInfo;
-
+        
         $this->info = $info->info;
         $this->avatar_id = $info->avatar_id;
-
-       
+        
+        
         $this->nickname = $this->_user->nickname;
         parent::__construct($config);
     }
@@ -40,13 +41,13 @@ class UserDataForm extends Model
     {
         if ($this->validate()) {
             $this->_user->nickname = $this->nickname;
-
+            
             $info = $this->_user->userInfo;
-
+            
             $info->info = $this->info;
             $info->avatar_id = $this->avatar_id;
-           
-           
+            
+            
             if ($this->_user->validate()) {
                 $this->_user->save();
                 return true;
@@ -57,8 +58,8 @@ class UserDataForm extends Model
         }
     }
     /**
-     * {@inheritdoc}
-     */
+    * {@inheritdoc}
+    */
     public function rules()
     {
         return [
@@ -68,5 +69,5 @@ class UserDataForm extends Model
             [['avatar_id'], 'exist', 'skipOnError' => true, 'targetClass' => File::className(), 'targetAttribute' => ['avatar_id' => 'id']],
         ];
     }
-
+    
 }
