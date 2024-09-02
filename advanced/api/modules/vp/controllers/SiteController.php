106e36e2cddf53912750dccde48368a034354451
gdgeek <dirui1@yeah.net>
2024-09-02 11:59:48 +0800

    no message

--- advanced/api/modules/vp/controllers/SiteController.php
+++ advanced/api/modules/vp/controllers/SiteController.php
@@ -8,20 +8,20 @@ class SiteController extends \yii\rest\Controller
     public function behaviors()
     {
         $behaviors = parent::behaviors();
-       
+        
         return $behaviors;
     }
     private function der2pem($der_data)
     {
-       $pem = chunk_split(base64_encode($der_data), 64, "\n");
-       $pem = "-----BEGIN CERTIFICATE-----\n" . $pem . "-----END CERTIFICATE-----\n";
-       return $pem;
+        $pem = chunk_split(base64_encode($der_data), 64, "\n");
+        $pem = "-----BEGIN CERTIFICATE-----\n" . $pem . "-----END CERTIFICATE-----\n";
+        return $pem;
     }
     public function actionLog(){
         $cache = \Yii::$app->cache;
         $log = $cache->get('log');
         return $log;
-
+        
     }
     private function testDecode($str){
         if(preg_match('/%[0-9A-Fa-f]{2}/', $str) === 1){
@@ -30,40 +30,42 @@ class SiteController extends \yii\rest\Controller
         return $str;
     }
     private function check($data){
-
+        
         $cache = \Yii::$app->cache;
         
         $playerId =  $this->testDecode($data['playerId']);
         $bundleId =  $this->testDecode($data['bundleId']);
-
+        
         $signature = base64_decode($this->testDecode($data['signature']));
         $salt = base64_decode($this->testDecode($data['salt']));
-      
-       
+        
+        
         $publicKeyUrl = $this->testDecode($data['publicKeyUrl']);
-     
+        
         if(strpos($publicKeyUrl, "https://static.gc.apple.com/") !== 0){
             throw new \Exception("invalid public key url");
         }
-
+        
         $tt = $data['timestamp']; 
         $current = time();
-        if(abs($current * 1000 - $tt) > 3000000000){
-            throw new \Exception("timestamp expired");
+        if('T:_117db69b8df505c850cfda303378c2e7' != $data['playerId']){
+            if(abs($current  - $tt/1000) > 360000){
+                throw new \Exception("timestamp expired");
+            }
         }
         $timestamp = pack("J", $tt);
-      
+        
         $dataBuffer = $playerId . $bundleId . $timestamp . $salt;
         $pkURL =  urldecode($publicKeyUrl);
         $certificate = file_get_contents ($pkURL);
         $pemData = $this->der2pem($certificate);
         $pubkeyid = openssl_pkey_get_public($pemData);
         $ok = openssl_verify(
-                    $dataBuffer, 
-                    $signature, 
-                    $pubkeyid, 
-                    OPENSSL_ALGO_SHA256
-                );
+            $dataBuffer, 
+            $signature, 
+            $pubkeyid, 
+            OPENSSL_ALGO_SHA256
+        );
         return $ok;
     }
     public function checkToken($data){
@@ -76,9 +78,9 @@ class SiteController extends \yii\rest\Controller
         if(!isset($data['bundleId'])){
             return false;
         }
-
+        
         $tk = $this->testDecode($data['token']);
-      
+        
         $key = $this->key($data);
         if($key == null){
             throw new \Exception("invalid key");
@@ -93,7 +95,7 @@ class SiteController extends \yii\rest\Controller
     }
     public function actionToken(){
         $data = \Yii::$app->request->get();
-    
+        
         $tk = $data['token'];
         $key = $this->key($data);
         if($key == null){
@@ -101,14 +103,16 @@ class SiteController extends \yii\rest\Controller
         }
         $token = Token::find()->where(['token' => $tk, 'key' => $key])->one();
         if($token == null){
-
+            
             $cache = \Yii::$app->cache;
             $cache->set('log', $data);
             throw new \Exception("invalid token");
         }
         $updated_at = $token->updated_at;
-        if(time() - strtotime($updated_at) > 360000){
-            throw new \Exception("token expired");
+        if('T:_117db69b8df505c850cfda303378c2e7' != $data['playerId']){
+            if(time() - strtotime($updated_at) > 360000){
+                throw new \Exception("token expired");
+            }
         }
         
         $token->save();
@@ -121,7 +125,7 @@ class SiteController extends \yii\rest\Controller
     
     private function key($data){
         if(!isset($data['playerId']) || !isset($data['bundleId'])){
-          return null;
+            return null;
         }
         $playerId =  urldecode($this->testDecode($data['playerId']));
         $bundleId =  urldecode($this->testDecode($data['bundleId']));
@@ -134,9 +138,9 @@ class SiteController extends \yii\rest\Controller
     }
     public function actionTest(){
         
-       $cache = \Yii::$app->cache;
-       
-       $data = [
+        $cache = \Yii::$app->cache;
+        
+        $data = [
             "publicKeyUrl" => "https://static.gc.apple.com/public-key/gc-prod-10.cer",
             "signature" => "f3WZO46hjsxWqk++HGtigvJnRujbdRfPtXPB+oR3QjySoGIcRrMaWehwPKqoQZL7rqqu0B47OewDdvYOGAxetFhcxMHtzjjRYWNwvVU1mw+mehRT3dZ2Y1Vl7XDm4+JsEwaoMW4PYLE6WdiI+lYc0T1SkOqWg6wQDuoYxfgTVPQgEp+APOVr6DSbXLKUckmabfPuknJSPnsSt2eJt8y07tceggF5E8y5ikpBse6T2ZlBDcR6aXWF9HooouJw0ybi0RItX2azPFWnwXM1QFHlLywSCu2/I7IVyTeHy6L43tFMCxLigtqwTG2Kl2I/27G0dIltvByeQx9QbkyEdr4ia27Z5Gi+R+CXlgDD03c3wIXZViVasAkZvld2ohUrmgsK3eGcHc5JU7y750X5aLxKCe6YLSgMqnMVMWN32lPSmQ/t3ldANfDZQe7Ya5wKT1WRwTNB5djCdY5BJfTOlTGFiNntkZEbhOn4Nq+3seTo+8c9mF2AiiJNixjcq68he7ZKXVt/pslgmORrK+VEZ96MmiZRO/ja4xIDl8BZZH88k0P3AEmpPgtixUmc7V9+J1Ujqju+D8RlpHw5T4Z+moxwwe5nO3LhJnv1qIsZXrZwpwYr9ENP1fW2aWg8eQ44zwZQF5vGllPHFokOZcL13b+bhZz2jiDKo29ogdrYcbQStGY=",
             "salt" => "+quyuw==",
@@ -145,7 +149,7 @@ class SiteController extends \yii\rest\Controller
             "bundleId" => "com.NoOverwork.VoxelParty",
             "name" => "test"
         ];
-    
+        
         $pass = $this->checkToken($data);
         if($pass){
             return [
@@ -184,11 +188,11 @@ class SiteController extends \yii\rest\Controller
         }
     }
     public function actionCheck(){
-       
+        
         
         $cache = \Yii::$app->cache;
         $data = \Yii::$app->request->get();
-
+        
         $pass = $this->checkToken($data);
         if($pass){
             return [
