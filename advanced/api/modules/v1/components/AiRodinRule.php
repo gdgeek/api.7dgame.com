545c055f4e8943137df21a32265c989f5f36a0fc
gdgeek <dirui1@yeah.net>
2024-10-07 10:30:03 +0800

    no message

--- advanced/api/modules/v1/components/AiRodinRule.php
+++ advanced/api/modules/v1/components/AiRodinRule.php
@@ -10,22 +10,15 @@ use yii\web\BadRequestHttpException;
 class AiRodinRule extends Rule
 {
     public $name = 'ai_rodin_rule';
-    //public $modelType = AiRodin::class;
-    //public $columnName = "user_id";
-    
+    public $columnName = "user_id";
+    public $modelType = AiRodin::class;
     public function execute($user, $item, $params)
     {
-        //$this->modelType = AiRodin::class;
-        //  $this->columnName = 'user_id';
-        // return parent::execute($user, $item, $params);
+        
         
         $request = Yii::$app->request;
-        
         $post = \Yii::$app->request->post();
         
-        
-        // $modelClass = $this->modelType;
-        // $columnName = $this->columnName;
         if(($request->isGet || $request->isPut ||$request->isDelete) && isset($params['id'])) {
             $model = AiRodin::findOne($params['id']);
             if($model->user_id === $user){
