b52d374f7d325d39d5dada02f45fd4de69466541
gdgeek <dirui1@yeah.net>
2024-10-07 08:01:40 +0800

    no message

--- advanced/api/modules/v1/components/AiRodinRule.php
+++ advanced/api/modules/v1/components/AiRodinRule.php
@@ -3,21 +3,38 @@
 namespace api\modules\v1\components;
 
 use api\modules\v1\models\AiRodin;
-use api\modules\v1\components\rule\UserDeriveRule;
 use Yii;
 use yii\rbac\Rule;
 use yii\web\BadRequestHttpException;
 
-class AiRodinRule extends UserDeriveRule
+class AiRodinRule extends Rule
 {
     public $name = 'ai_rodin_rule';
-    public $modelType = AiRodin::class;
-    public $columnName = "user_id";
+    //public $modelType = AiRodin::class;
+    //public $columnName = "user_id";
     
     public function execute($user, $item, $params)
     {
-        $this->modelType = AiRodin::class;
-        $this->columnName = 'user_id';
-        return parent::execute($user, $item, $params);
+        //$this->modelType = AiRodin::class;
+        //  $this->columnName = 'user_id';
+        // return parent::execute($user, $item, $params);
+        
+        $request = Yii::$app->request;
+        
+        $post = \Yii::$app->request->post();
+        
+        
+        // $modelClass = $this->modelType;
+        // $columnName = $this->columnName;
+        if(($request->isGet || $request->isPut ||$request->isDelete) && isset($params['id'])) {
+            $model = AiRodin::findOne($params['id']);
+            if($model->user_id === $user){
+                return true;
+            }
+            return false;
+        }else if($request->isPost) {
+            return true;
+        }
+        
     }
 }
