5561e0bec3d9add38750bfa808b4c243b0f48c28
gdgeek <dirui1@yeah.net>
2025-10-10 16:55:51 +0800

    no message

--- advanced/api/modules/v1/RefreshToken.php
+++ advanced/api/modules/v1/RefreshToken.php
@@ -1,13 +1,13 @@
 <?php
+/*
 namespace api\modules\v1;
 class RefreshToken extends \yii\redis\ActiveRecord
 {
-    /**
-     * @return array 此记录的属性列表
-     */
+    
     public function attributes()
     {
         return ['id', 'user_id', 'key'];
     }
 
 }
+*/
